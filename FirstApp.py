833a91f400e18890549ecfdc5f8361f64965994e
Konstantin Pozdin <lyagyshkalyaga@gmail.com>
2025-10-31 17:11:31 +0500

    Added a new line for entering our text

--- FirstApp.py
+++ FirstApp.py
@@ -1,18 +1,15 @@
 import tkinter as tk
 from random import randint, choice
 from tkinter import messagebox
-i=0
-def left_button_click():
-    global i
-    label.config(text="You`ve clicked")
-    i=i+1
-    label2 = tk.Label(window, text="Вы нажали на кнопку " + str(i) + " раз")
-    label2.pack(pady=20)
 window = tk.Tk()  # обращаемся к классу Tk из модуля tk
 window.title("My App")
+window.resizable(width = False, height = False)# отсуствие растягивания
 window.geometry("800x600")
-label = tk.Label(window,text="Text of app")
-label.pack(pady=10)
-button=tk.Button(window,text="Click",command=left_button_click)
-button.pack(pady=20)
+window["bg"]="orange"
+
+note=tk.Label(window, text="Добавить заметку",font=("Arial",10),bg="white")
+note.place(x= 400,y = 25)
+EnterNote=tk.Entry(fg="black",width=50)# поле ввода
+EnterNote.place(x=300,y=65)
+
 window.mainloop()
