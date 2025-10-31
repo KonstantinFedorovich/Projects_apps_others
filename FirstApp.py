cfb273e92cfe36663d884ba18498fb5bcde7b1c6
Konstantin Pozdin <lyagyshkalyaga@gmail.com>
2025-10-31 16:35:52 +0500

    First usage of tk-module

--- FirstApp.py
+++ FirstApp.py
@@ -1,1 +1,18 @@
-print('Hello, world!')
+import tkinter as tk
+from random import randint, choice
+from tkinter import messagebox
+i=0
+def left_button_click():
+    global i
+    label.config(text="You`ve clicked")
+    i=i+1
+    label2 = tk.Label(window, text="Вы нажали на кнопку " + str(i) + " раз")
+    label2.pack(pady=20)
+window = tk.Tk()  # обращаемся к классу Tk из модуля tk
+window.title("My App")
+window.geometry("800x600")
+label = tk.Label(window,text="Text of app")
+label.pack(pady=10)
+button=tk.Button(window,text="Click",command=left_button_click)
+button.pack(pady=20)
+window.mainloop()
